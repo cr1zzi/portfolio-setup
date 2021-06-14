a6b4565ead2c1652c3238089cd89c5350fb15638
cr1zzi <cr1zzi112@gmail.com>
2021-06-14 16:33:01 +0300

    edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header-wraper">
       <div className="main-info">
         <h1>
-          Welcome <p></p> I'm Cristiann
+          Welcome <p></p> I'm Cristian
         </h1>
         <Typed
           className="typed-text"
